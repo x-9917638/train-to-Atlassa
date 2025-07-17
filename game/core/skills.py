3f3b979018fe4e9c589fd2ff2d5787230621e0e1
x-9917638 <vincent.yeung2@education.nsw.gov.au>
2025-07-17 18:30:02 +1000

    Nerf damage scaling for skills

--- game/core/skills.py
+++ game/core/skills.py
@@ -44,7 +44,7 @@ class Skill:
             results = []
             for target in targets:
                 if self.target in [SkillTarget.SINGLE_ENEMY, SkillTarget.ALL_ENEMIES]:
-                    damage = (user.attack // 4) * self.power
+                    damage = (user.attack // 6) * self.power
                     damage = target.take_damage(damage)
                     results.append(f"{user.name} uses {self.name} on {target.name} for {damage} damage!")
                 elif self.target in [SkillTarget.SELF, SkillTarget.SINGLE_ALLY, SkillTarget.ALL_ALLIES]:
